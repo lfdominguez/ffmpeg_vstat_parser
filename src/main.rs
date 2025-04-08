ef7a53cd3440eaa1cd731a007f12135ca660292e
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2025-04-08 04:10:02 -0300

    fix wait handler
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-use std::io::Read;
-use std::sync::mpsc;
-use ipipe::OnCleanup;
-use log::{debug, info, trace, warn};
-use crate::args::{OutputType, ParserMode, APP_ARGS};
+use crate::args::{APP_ARGS, OutputType, ParserMode};
 use crate::modes::ProcessLog;
 use crate::parser::{LineInfo, ParseInfo};
+use ipipe::OnCleanup;
+use log::{debug, info, trace, warn};
+use std::io::Read;
+use std::sync::{Arc, RwLock, mpsc};
 
 mod args;
 mod modes;
 mod parser;
 pub mod regexes;
 
-
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    info!("Reading from pipe file '{}' using mode '{:?}'", APP_ARGS.fifo_file_in, APP_ARGS.parser_mode);
+    info!(
+        "Reading from pipe file '{}' using mode '{:?}'",
+        APP_ARGS.fifo_file_in, APP_ARGS.parser_mode
+    );
 
-    let rx_test = ipipe::Pipe::open(std::path::Path::new(&APP_ARGS.fifo_file_in), OnCleanup::NoDelete);
+    let rx_test = ipipe::Pipe::open(
+        std::path::Path::new(&APP_ARGS.fifo_file_in),
+        OnCleanup::NoDelete,
+    );
 
     if let Ok(mut rx) = rx_test {
         debug!("Started read process");
@@ -37,11 +42,14 @@ fn main() -> anyhow::Result<()> {
                     }
                 }
             }
-        });        
+        });
+
+        let buffer_line_writter: Arc<RwLock<String>> = Arc::new(RwLock::new(String::new()));
+        let buffer_line_reader: Arc<RwLock<String>> = Arc::clone(&buffer_line_writter);
 
         std::thread::spawn(move || {
             debug!("Spawn write from fifo tokio thread");
-            
+
             let mut incomplete_line = String::new();
 
             loop {
@@ -55,93 +63,54 @@ fn main() -> anyhow::Result<()> {
                             let mut start = 0;
                             for (i, &byte) in msg_bytes.iter().enumerate() {
                                 if byte == b'\n' {
-                                    let line = format!("{}{}", incomplete_line, String::from_utf8_lossy(&msg_bytes[start..i]));
+                                    let line = format!(
+                                        "{}{}",
+                                        incomplete_line,
+                                        String::from_utf8_lossy(&msg_bytes[start..i])
+                                    );
 
-                                    let parser_info_result = match APP_ARGS.parser_mode {
-                                        ParserMode::Raw => Some(LineInfo {
-                                            raw_line: line,
-                                            parse_info: None
-                                        }),
-
-                                        ParserMode::FfmpegVstatV2 => {
-                                            let ffmpeg_info_result = parser::parse_ffmpeg_vstat(&line);
-
-                                            if let Ok(ffmpeg_info) = ffmpeg_info_result {
-                                                Some(LineInfo {
-                                                    raw_line: line,
-                                                    parse_info: Some(ParseInfo::Ffmpeg(Box::new(ffmpeg_info)))
-                                                })
-                                            } else {
-                                                debug!("Fail parsing ffmpeg vstat line: {}", ffmpeg_info_result.err().unwrap());
-
-                                                None
-                                            }
+                                    if APP_ARGS.wait_msec > 0 {
+                                        if let Ok(mut w_line) = buffer_line_writter.write() {
+                                            *w_line = line;
                                         }
-
-                                        ParserMode::GigaTools => {
-                                            parser::parse_gigatools(&line).map(|gigatool_info| LineInfo {
-                                                raw_line: line,
-                                                parse_info: Some(ParseInfo::GigaTools(Box::new(gigatool_info))),
-                                            })
-                                        }
-                                        
-                                        ParserMode::TspContinuity => {
-                                            parser::parse_tsp_continuity(&line).map(|tsp_info| LineInfo {
-                                                raw_line: line,
-                                                parse_info: Some(ParseInfo::TspContinuity(Box::new(tsp_info))),
-                                            })
-                                        }
-
-                                        ParserMode::TspHistory => {
-                                            parser::parse_tsp_history(&line).map(|tsp_info| LineInfo {
-                                                raw_line: line,
-                                                parse_info: Some(ParseInfo::TspHistory(Box::new(tsp_info))),
-                                            })
-                                        }
-                                    };
-
-                                    if let Some(parser_info) = parser_info_result {
-                                        match &APP_ARGS.command {
-                                            OutputType::FifoOut(fifo_out_args) => {
-                                                let processor_test = modes::fifo_out::FifoOut::new(&fifo_out_args.fifo_output);
-
-                                                if let Ok(mut processor) = processor_test {
-                                                    if let Err(e) = processor.process_log(parser_info) {
-                                                        warn!("Error processing line: {}", e.to_string());
-                                                    }
-                                                } else {
-                                                    panic!("Error creating processor: {}", processor_test.err().unwrap())
-                                                }
-                                            }
-                                            OutputType::HttpPost(http_args) => {
-                                                let mut processor = modes::http_out::HttpOut::new(http_args.uri_endpoint.clone(), http_args.data_format.clone());
-
-                                                if let Err(e) = processor.process_log(parser_info) {
-                                                    warn!("Error processing line: {}", e.to_string());
-                                                }
-                                            }
-                                        };
-
+                                    } else {
+                                        process_line(line);
                                     }
-                                    
+
                                     incomplete_line.clear();
                                     start = i + 1;
                                 }
                             }
 
                             if start < msg_bytes.len() {
-                                incomplete_line.push_str(&String::from_utf8_lossy(&msg_bytes[start..]));
+                                incomplete_line
+                                    .push_str(&String::from_utf8_lossy(&msg_bytes[start..]));
                             }
 
                             trace!("Writed: '{}'", msg_string);
                         } else {
                             warn!("Ignoring log line: '{:?}'", msg_string_test.err());
                         }
-                    },
+                    }
                     Err(err) => trace!("Error on FIFO channel: {err}"),
                 }
             }
         });
+
+        if APP_ARGS.wait_msec > 0 {
+            std::thread::spawn(move || {
+                loop {
+                    if let Ok(line) = buffer_line_reader.read() {
+                        if line.len() > 0 {
+                            process_line(line.to_string());
+                        }
+                    }
+
+                    log::debug!("Waiting for process line: {} ms", APP_ARGS.wait_msec);
+                    std::thread::sleep(std::time::Duration::from_millis(APP_ARGS.wait_msec as u64));
+                }
+            });
+        }
 
         loop {
             if let Ok(exists) = std::fs::exists(APP_ARGS.fifo_file_in.clone()) {
@@ -151,11 +120,89 @@ fn main() -> anyhow::Result<()> {
                 }
             }
 
-            debug!("Wait for 1s to check again for {} exists.", APP_ARGS.fifo_file_in);
+            debug!(
+                "Wait for 1s to check again for {} exists.",
+                APP_ARGS.fifo_file_in
+            );
 
             std::thread::sleep(std::time::Duration::from_secs(1));
         }
     } else {
         anyhow::bail!("Error opening read of tunnel file")
+    }
+}
+
+fn process_line(line: String) {
+    log::debug!("Processing line: {}", line);
+
+    let parser_info_result = match APP_ARGS.parser_mode {
+        ParserMode::Raw => Some(LineInfo {
+            raw_line: line,
+            parse_info: None,
+        }),
+
+        ParserMode::FfmpegVstatV2 => {
+            let ffmpeg_info_result = parser::parse_ffmpeg_vstat(&line);
+
+            if let Ok(ffmpeg_info) = ffmpeg_info_result {
+                Some(LineInfo {
+                    raw_line: line,
+                    parse_info: Some(ParseInfo::Ffmpeg(Box::new(ffmpeg_info))),
+                })
+            } else {
+                debug!(
+                    "Fail parsing ffmpeg vstat line: {}",
+                    ffmpeg_info_result.err().unwrap()
+                );
+
+                None
+            }
+        }
+
+        ParserMode::GigaTools => parser::parse_gigatools(&line).map(|gigatool_info| LineInfo {
+            raw_line: line,
+            parse_info: Some(ParseInfo::GigaTools(Box::new(gigatool_info))),
+        }),
+
+        ParserMode::TspContinuity => parser::parse_tsp_continuity(&line).map(|tsp_info| LineInfo {
+            raw_line: line,
+            parse_info: Some(ParseInfo::TspContinuity(Box::new(tsp_info))),
+        }),
+
+        ParserMode::TspHistory => parser::parse_tsp_history(&line).map(|tsp_info| LineInfo {
+            raw_line: line,
+            parse_info: Some(ParseInfo::TspHistory(Box::new(tsp_info))),
+        }),
+    };
+
+    if let Some(parser_info) = parser_info_result {
+        log::debug!("Sending to out the parsed info");
+
+        match &APP_ARGS.command {
+            OutputType::FifoOut(fifo_out_args) => {
+                let processor_test = modes::fifo_out::FifoOut::new(&fifo_out_args.fifo_output);
+
+                if let Ok(mut processor) = processor_test {
+                    if let Err(e) = processor.process_log(parser_info) {
+                        warn!("Error processing line: {}", e.to_string());
+                    }
+                } else {
+                    panic!(
+                        "Error creating processor: {}",
+                        processor_test.err().unwrap()
+                    )
+                }
+            }
+            OutputType::HttpPost(http_args) => {
+                let mut processor = modes::http_out::HttpOut::new(
+                    http_args.uri_endpoint.clone(),
+                    http_args.data_format.clone(),
+                );
+
+                if let Err(e) = processor.process_log(parser_info) {
+                    warn!("Error processing line: {}", e.to_string());
+                }
+            }
+        };
     }
 }
--- src/modes/http_out.rs
+++ src/modes/http_out.rs
@@ -1,71 +1,48 @@
-use reqwest::blocking::{Client};
-use crate::args::{HttpFormat, APP_ARGS};
+use crate::args::{APP_ARGS, HttpFormat};
 use crate::parser::LineInfo;
+use reqwest::blocking::Client;
 
 pub struct HttpOut {
     http_endpoint: String,
     http_format: HttpFormat,
-    wait_msec: Option<i64>,
-    last_sent_milliseconds: i64,
-    last_fail_milliseconds: Option<i64>
 }
 
 impl HttpOut {
     pub fn new(http_endpoint: String, http_format: HttpFormat) -> Self {
         Self {
             http_endpoint,
-            wait_msec: if APP_ARGS.wait_msec == 0 { None } else { Some(APP_ARGS.wait_msec) },
-            last_sent_milliseconds: chrono::Utc::now().timestamp_millis(),
             http_format,
-            last_fail_milliseconds: None
         }
     }
 }
 
 impl crate::modes::ProcessLog for HttpOut {
     fn process_log(&mut self, line_info: LineInfo) -> anyhow::Result<()> {
-
-        let current_milliseconds = chrono::Utc::now().timestamp_millis();
-
-        if let Some(wait_msec) = self.wait_msec {
-            if current_milliseconds - self.last_sent_milliseconds < wait_msec {
-                return Ok(());
-            }
-        }
-
-        if let Some(last_fail_milliseconds) = self.last_fail_milliseconds {
-            if current_milliseconds - last_fail_milliseconds < 10000 {
-                return Ok(());
-            }
-        }
+        log::debug!("Processing http out");
 
         let request_builder = Client::new().post(self.http_endpoint.clone());
 
         let request_builder = match self.http_format {
-            
-            HttpFormat::Json => if let Some(parse_info) = line_info.parse_info {
-                request_builder.json(&parse_info)
-            } else {
-                request_builder.json(&format!("{{\"raw\": {} }}", line_info.raw_line))
+            HttpFormat::Json => {
+                if let Some(parse_info) = line_info.parse_info {
+                    request_builder.json(&parse_info)
+                } else {
+                    request_builder.json(&format!("{{\"raw\": {} }}", line_info.raw_line))
+                }
             }
-            
+
             HttpFormat::MsgPack => {
                 let msgpack_data = rmp_serde::to_vec(&line_info.parse_info)?;
-                
+
                 request_builder
                     .header("Content-Type", "application/x-msgpack")
                     .body(msgpack_data)
             }
         };
 
-        request_builder.send()
-            .map(|_| {
-                self.last_sent_milliseconds = chrono::Utc::now().timestamp_millis();
-                self.last_fail_milliseconds = None;
-            })
-            .inspect_err(|_| {
-                self.last_fail_milliseconds = Some(chrono::Utc::now().timestamp_millis());
-            })?;
+        request_builder.send().inspect_err(|_| {
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        })?;
 
         Ok(())
     }
