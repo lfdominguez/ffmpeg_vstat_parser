a6ab67bd107e5ef853614a9cf13de404cc7340f0
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2025-04-11 03:49:09 -0300

    fix: add support for extra characters in old versions of ffmpeg
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- src/regexes.rs
+++ src/regexes.rs
@@ -1,21 +1,23 @@
 use once_cell::sync::Lazy;
 
 pub static FFMPEG_VSTAT_REGEX: Lazy<regex::Regex> = Lazy::new(|| {
-    regex::Regex::new(r"out=\s+(?<out>\d+)\s+st=\s+(?<st>\d+)\s+frame=\s+(?<frame>\d+)\s+q=\s+(?<q>\d+\.\d+)\s+f_size=\s+(?<f_size>\d+)\s+s_size=\s+(?<s_size>\d+)[kK]i?B\s+time=\s+(?<time>\d+\.\d+)\s+br=\s+(?<br>\d+\.\d+)kbits/s\s+avg_br=\s+(?<avg_br>\d+\.\d+)kbits/s\s+type=\s+(?<type>[a-zA-z]+)").unwrap()
+    regex::Regex::new(r"out=\s+(?<out>\d+)\s+st=\s+(?<st>\d+)\s+frame=\s+(?<frame>\d+)\s+q=\s+(?<q>\-?\d+\.\d+)\s+f_size=\s+(?<f_size>\d+)\s+s_size=\s+(?<s_size>\d+)[kK]i?B\s+time=\s+(?<time>\d+\.\d+)\s+br=\s+(?<br>\d+\.\d+)kbits/s\s+avg_br=\s+(?<avg_br>\d+\.\d+)kbits/s\s+type=\s+(?<type>[a-zA-z]+|\?)").unwrap()
 });
 
 pub static TSP_RE_CONTINUITY: Lazy<regex::Regex> = Lazy::new(|| {
-    regex::Regex::new(r"continuity:.+ PID: (?<program_pid>.+), missing (?<missing_count>\d+) packet").unwrap()
+    regex::Regex::new(
+        r"continuity:.+ PID: (?<program_pid>.+), missing (?<missing_count>\d+) packet",
+    )
+    .unwrap()
 });
 
-pub static TSP_RE_HISTORY: Lazy<regex::Regex> = Lazy::new(|| {
-    regex::Regex::new(r"\* history: (?<packet_number>\d+): (?<message>.+)").unwrap()
-});
+pub static TSP_RE_HISTORY: Lazy<regex::Regex> =
+    Lazy::new(|| regex::Regex::new(r"\* history: (?<packet_number>\d+): (?<message>.+)").unwrap());
 
 pub static TSP_RE_TDT: Lazy<regex::Regex> = Lazy::new(|| {
-    regex::Regex::new(r"TDT: (?<datetime>[0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})").unwrap()
+    regex::Regex::new(r"TDT: (?<datetime>[0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})")
+        .unwrap()
 });
 
-pub static TSP_RE_PID_MODE: Lazy<regex::Regex> = Lazy::new(|| {
-    regex::Regex::new(r"PID \d+ \((?<pid>0x\d+)\) (?<action>[^,]+)").unwrap()
-});
+pub static TSP_RE_PID_MODE: Lazy<regex::Regex> =
+    Lazy::new(|| regex::Regex::new(r"PID \d+ \((?<pid>0x\d+)\) (?<action>[^,]+)").unwrap());
